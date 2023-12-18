4cc6e85300807ef77f4274809ce7a260417fbe67
Kapil <kapil@Kapils-MacBook-Air.local>
2023-12-18 18:21:42 +0530

    add

---
+++ lib/my_notification/my_cart_page.dart
@@ -0,0 +1,79 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:kapil11/my_cart/my_cart_result.dart';
+import 'package:kapil11/utility/api_service.dart';
+import 'package:kapil11/widgets/custom_app_bar2.dart';
+
+class MyCartClass extends StatefulWidget {
+  const MyCartClass({super.key});
+
+  @override
+  State<MyCartClass> createState() => _MyCartClassState();
+}
+
+class _MyCartClassState extends State<MyCartClass> {
+  late Future<List<MyCartItem>> _dataListFuture;
+  @override
+  void initState() {
+    super.initState();
+    _dataListFuture = fetchMedicineData();
+  }
+
+  Future<List<MyCartItem>> fetchMedicineData() async {
+    final response = await ApiService.my_cart_api();
+
+    if (response.statusCode == 200) {
+      var mybody = json.decode(response.body);
+      List<dynamic> data = mybody[0]["items"];
+      List<MyCartItem> MyCartItems =
+          data.map((item) => MyCartItem.fromJson(item)).toList();
+      return MyCartItems;
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar2(page_title: "Search Medicine"),
+      body: Column(
+        children: [
+          FutureBuilder(
+            future: _dataListFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Container(
+                  height: MediaQuery.of(context).size.height - 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Container(
+                          height: 50,
+                          width: 50,
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                      Container(height: 11),
+                      Text("Loading....")
+                    ],
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                List<MyCartItem> dataList = snapshot.data as List<MyCartItem>;
+                return Container(
+                    height: MediaQuery.of(context).size.height - 150,
+                    child: MyCartList(dataList));
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/my_order/my_cart_result.dart
@@ -0,0 +1,134 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:kapil11/medicine_details/medicine_details_page.dart';
+
+class MyCartItem {
+  final String itemId;
+  final String itemCode;
+  final int itemQuantity;
+  final int itemOrderQuantity;
+  final String itemImage;
+  final String itemName;
+  final String itemPacking;
+  final String itemExpiry;
+  final String itemCompany;
+  final String itemScheme;
+  final double itemMargin;
+  final int itemFeatured;
+  final double itemPrice;
+  final double itemQuantityPrice;
+  final String itemDateTime;
+  final String itemModalNumber;
+
+  MyCartItem({
+    required this.itemId,
+    required this.itemCode,
+    required this.itemQuantity,
+    required this.itemOrderQuantity,
+    required this.itemImage,
+    required this.itemName,
+    required this.itemPacking,
+    required this.itemExpiry,
+    required this.itemCompany,
+    required this.itemScheme,
+    required this.itemMargin,
+    required this.itemFeatured,
+    required this.itemPrice,
+    required this.itemQuantityPrice,
+    required this.itemDateTime,
+    required this.itemModalNumber,
+  });
+
+  factory MyCartItem.fromJson(Map<String, dynamic> json) {
+    return MyCartItem(
+      itemId: json['item_id'],
+      itemCode: json['item_code'],
+      itemQuantity: int.parse(json['item_quantity']),
+      itemOrderQuantity: int.parse(json['item_order_quantity']),
+      itemImage: json['item_image'],
+      itemName: json['item_name'],
+      itemPacking: json['item_packing'],
+      itemExpiry: json['item_expiry'],
+      itemCompany: json['item_company'],
+      itemScheme: json['item_scheme'],
+      itemMargin: double.parse(json['item_margin']),
+      itemFeatured: int.parse(json['item_featured']),
+      itemPrice: double.parse(json['item_price']),
+      itemQuantityPrice: double.parse(json['item_quantity_price']),
+      itemDateTime: (json['item_datetime']),
+      itemModalNumber: json['item_modalnumber'],
+    );
+  }
+}
+
+class MyCartList extends StatelessWidget {
+  final List<MyCartItem> dataList;
+
+  MyCartList(this.dataList);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: dataList.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              print(dataList[index].itemName);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MedicineDetailsClass(
+                      itemCode: dataList[index].itemCode,
+                      itemName: dataList[index].itemName,
+                      itemImage: dataList[index].itemImage),
+                ),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                // Set your desired color
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Image.network(
+                    dataList[index].itemImage,
+                    height: 64, // Specify the height you want
+                    width: 64, // Specify the width you want
+                    fit: BoxFit.cover,
+                  ),
+                  Container(
+                    width: 11,
+                  ),
+                  Container(
+                    width: 300,
+                    alignment: Alignment.centerLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          dataList[index].itemName,
+                          textAlign: TextAlign.left,
+                        ),
+                        Text(
+                          dataList[index].itemCompany,
+                          textAlign: TextAlign.left,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
